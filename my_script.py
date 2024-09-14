0f03bacf7bacea7b571b9578db8910e64a7d158c
chuksmomoh2003 <obi.chuka@yahoo.com>
2024-09-13 23:58:40 -0600

    updated my_script.py

--- my_script.py
+++ my_script.py
@@ -1,2 +1,3 @@
 print('I will get a job soonest. ISEEEE') 
-print('Chimebuka and Ogwugwu Ikwu nne m amaka') 
+print('Chimebuka and Ogwugwu Ikwu nne m amaka')
+print('I love my Ezumezu Muo du m bia uwa') 
